5b02dd525bc941041a41f1b829090ae8c3dd5501
LucasBiazi <lucas.ferreira@novaandradina.org>
2020-10-20 22:37:14 -0400

    Progress on script.js.

--- Calendar/Calendar_Web_App/script/script.js
+++ Calendar/Calendar_Web_App/script/script.js
@@ -1,11 +1,11 @@
-// Returns date object with more properties
-const get_date = () => {
-  const amount_of_days = (year, month) =>
-    new Date(year, month + 1, 0).getDate();
+// Returns the amount of days in a month.
+const amount_of_days = (year, month) => new Date(year, month + 1, 0).getDate();
 
-  const first_day_week_for_month = (year, month) =>
-    new Date(year, month, 1).getDay();
+const first_day_week_for_month = (year, month) =>
+  new Date(year, month, 1).getDay();
 
+// Returns current date object with more properties.
+const get_date = (date_year, date_month) => {
   const month_names = [
     "January",
     "February",
@@ -21,7 +21,7 @@ const get_date = () => {
     "December",
   ];
 
-  const date_object = new Date();
+  const date_object = new Date(date_year, date_month);
   const date = {
     year: date_object.getFullYear(),
     month: date_object.getMonth(),
@@ -38,17 +38,87 @@ const get_date = () => {
   return date;
 };
 
-
-function print_month_days() {
-  get_date();
+// Prints year + month on the html after 1 milisecond..
+function print_year_and_month(date_year, date_month) {
+  setTimeout(() => {
+    const date = get_date(date_year, date_month);
+    document.getElementById("year_title").textContent = date.year;
+    document.getElementById("month_title").textContent = date.month_name;
+  }, 1);
 }
 
-// Loads print_month_days function as soon as the page loads.
+// Creates the table after 1 milisecond.
+function create_table() {
+  setTimeout(() => {
+    const table = document.getElementById("days");
+    // Creates 6 rows.
+    for (let i = 0; i < 6; i++) {
+      let current_row = table.insertRow(1 + i);
+      // Creates 7 cells.
+      for (let x = 0; x < 7; x++) {
+        current_row.insertCell(x);
+      }
+    }
+  }, 1);
+}
+
+// Clean all data on the table, then create it again.
+function reset_table() {
+  const table = document.getElementById("days");
+  for (let i = 0; i < 6; i++) {
+    table.deleteRow(1 + i);
+  }
+  create_table();
+}
+
+// Run through a specific row, and all its cells.
+function populate_row(row_number, starting_cell, cell_value, class_name) {
+  setTimeout(() => {
+    const table = document.getElementById("days");
+    for (let i = 0; i < table.rows[row_number].cells.length; i++) {
+      table.rows[row_number].cells[starting_cell].textContent = cell_value;
+      table.rows[row_number].cells[starting_cell].classList.add(class_name);
+    }
+  }, 1);
+}
+
+// Populates the table. AD = Amount of days. AC = Amount of cells.
+function populate_table(date_year, date_month) {
+  setTimeout(() => {
+    const date = get_date(date_year, date_month);
+    const AC_current_month = 7 - date.day_of_the_week_in_which_the_month_begins;
+    const AC_last_month = 7 - AC_current_month;
+    let AD_last_month = get_date(date.year, date.month - 1).amount_of_days;
+    let day_counter = 1;
+
+    // first row.
+    for (let i = 0; i < AC_last_month; i++) {
+      populate_row(1, AC_last_month - i - 1, AD_last_month - i, "other_month");
+    }
+    for (i = 0; i < AC_current_month; i++) {
+      populate_row(1, 7 - i - 1, AC_current_month - i, "td");
+      day_counter++;
+    }
+
+    // For the rest of them.
+    for (i = 2; i < 7; i++) {
+      
+    }
+  }, 1);
+}
+
+function main() {
+  print_year_and_month(new Date().getFullYear(), new Date().getMonth());
+  create_table();
+  populate_table(new Date().getFullYear(), new Date().getMonth());
+}
+
+// Loads main function as soon as the page loads.
 function trigger_script() {
   if (document.readyState !== "loading") {
-    print_month_days();
+    main();
   } else {
-    document.addEventListener("DOMContentLoaded", print_month_days());
+    document.addEventListener("DOMContentLoaded", main());
   }
 }
 
